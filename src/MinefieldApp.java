1c2250fefc2a73fa0899a38cb38f109d780c7382
ajboard13 <aaronjboard@live.com>
2017-07-12 13:14:22 -0400

    Made a print out statement in main

--- src/MinefieldApp.java
+++ src/MinefieldApp.java
@@ -1,8 +1,7 @@
-/**
- * Created by Nathan Saleniuc on 7/12/2017.
+/*
  */
 public class MinefieldApp {
     public static void main(String[] args) {
-
+        System.out.println("Aaron's change");
     }
 }
