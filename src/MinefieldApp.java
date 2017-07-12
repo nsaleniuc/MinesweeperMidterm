118ec48a2b7e6edd99bf356e7f1cfaa65081b664
thesuz88 <stevensuzio@gmail.com>
2017-07-12 13:48:57 -0400

    Created menu method and userSelection method

--- src/MinefieldApp.java
+++ src/MinefieldApp.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 /*
  */
 public class MinefieldApp {
     public static void main(String[] args) {
-        System.out.println("Aaron's change");
+        Scanner scan = new Scanner(System.in);
+
+        getDifficultyToPlay(scan);
+
+        getUserSelection(scan);
+
     }
+
+    private static void getUserSelection(Scanner scan) {
+        System.out.println("\nSelect which row and column you would like to reveal: ");
+        System.out.print("Row: ");
+        int rowSelected = scan.nextInt();
+        System.out.print("Column: ");
+        int colSelected = scan.nextInt();
+    }
+
+    private static void getDifficultyToPlay(Scanner scan) {
+        System.out.println("Welcome to Minesweeper!\nChoose the level of difficulty to play: ");
+        System.out.print("Beginner - 1\nIntermediate - 2\nExpert - 3\nChoose 1, 2, or 3: ");
+        int menuChoice = scan.nextInt();
+    }
+
 }
